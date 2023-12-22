b23f1c986637555f84d0ac50bd525153b6cc6acb
Ivan_Kariev <iva.karev@gmail.com>
2023-12-22 05:15:20 +0200

    Update test_Lab2.py

--- test_Lab2.py
+++ test_Lab2.py
@@ -3,7 +3,8 @@ from Lab2 import DitsadokQueue
 # -*- coding: utf-8 -*-
 import sys
 import codecs
-sys.stdout = codecs.getwriter("iso-8859-1")(sys.stdout, 'xmlcharrefreplace')class TestDitsadokQueue(unittest.TestCase):
+sys.stdout = codecs.getwriter("iso-8859-1")(sys.stdout, 'xmlcharrefreplace')
+class TestDitsadokQueue(unittest.TestCase):
     
     def setUp(self):
         self.ditsadok_queue = DitsadokQueue()
