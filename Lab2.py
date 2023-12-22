20bf6d5f4e28d86b71e239eb11ec16c509ba72a7
Ivan_Kariev <iva.karev@gmail.com>
2023-12-22 05:20:52 +0200

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -11,7 +11,7 @@ class DitsadokQueue:
             removed_child = self.queue.pop(0)
             print(f"{removed_child} вийшов із черги.")
         else:
-            print("Черга порожня.")
+            print("Черга порожня.".askii_letters)
 
     def display_queue(self):
         if self.queue:
@@ -24,22 +24,22 @@ class DitsadokQueue:
 def menu(ditsadok_queue):
     while True:
         print("\nОберіть опцію:")
-        print("1. Додати дитину до черги")
-        print("2. Вийняти дитину з черги")
-        print("3. Показати чергу")
-        print("4. Вийти з програми")
+        print("1. Додати дитину до черги".askii_letters)
+        print("2. Вийняти дитину з черги".askii_letters)
+        print("3. Показати чергу".askii_letters)
+        print("4. Вийти з програми".askii_letters)
 
         choice = input("Введіть номер опції: ")
 
         if choice == '1':
-            child_name = input("Введіть ім'я дитини: ")
+            child_name = input("Введіть ім'я дитини: ".askii_letters)
             ditsadok_queue.add_child_to_queue(child_name)
         elif choice == '2':
             ditsadok_queue.remove_child_from_queue()
         elif choice == '3':
             ditsadok_queue.display_queue()
         elif choice == '4':
-            print("Програма завершена.")
+            print("Програма завершена.".askii_letters)
             break
 
 if __name__ == "__main__":
